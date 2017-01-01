b44317d69c045955ed584e4a99f99ac1cb23ad2b
dario <dario@marvin>
2017-01-01 23:47:28 +0100

    inform message

--- src/device01.py
+++ src/device01.py
@@ -76,6 +76,28 @@ def deviceID(oui="06DA41", manufacturer="Device and co.", product_class="IAD X-2
     return device
 
 
+def inform(events=['0 BOOTSTRAP', '1 BOOT']):
+    
+    """ Create inform request and return etree element
+    
+    Keyword arguments:
+    events - list of event codes (strings)
+    """
+    
+    cwmp_inform = etree.Element(CWMP+"Inform")
+    event = etree.Element("Event")
+    event.attrib[SOAP+"arrayType"] = "cwmp:EventStruct[" + str(len(events)).zfill(2) + "]"
+    for e in events:
+        event_struct = etree.Element("EventStruct")
+        etree.SubElement(event_struct, "EventCode").text = e
+        etree.SubElement(event_struct, "CommandKey")
+        event.append(event_struct)
+    
+    cwmp_inform.append(deviceID())
+    cwmp_inform.append(event)
+    
+    return cwmp_inform
+        
 def soap_message():
     
     """ Returns sample TR-069 inform message as etree """
@@ -83,7 +105,7 @@ def soap_message():
     message = soap_envelope()
     message.append(soap_header())
     body = soap_body()
-    body.append(deviceID())
+    body.append(inform())
     message.append(body)
     
     return message
