ae7557469211bb024148bd1b7713ca708c6c3703
protu <protulipac@gmail.com>
2017-01-09 16:11:29 +0100

    Beginning of communication

---
+++ src/devreq.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from device import cpe
+import requests
+
+url = 'http://localhost:8080'
+req = requests.post(url, cpe.tostring(),
+                    headers={'Content-Type': 'text/xml; charset=ISO-8859-1'})
+for key in req.headers:
+    print(key+': '+req.headers[key])
+print(req.text)
--- src/tstpyrmid.py
+++ src/tstpyrmid.py
@@ -5,14 +5,16 @@ from pyramid.view import view_config
 from acs import response
 
 
-@view_config(route_name='hello', renderer='string')
-def hello_world(request):
+@view_config(route_name='cwmp', renderer='string')
+def cwmp(request):
+    request.response.content_type = 'text/xml; charset=UTF-8'
     rsp = response.tostring()
+    print(request)
     return rsp
 
 if __name__ == '__main__':
     config = Configurator()
-    config.add_route('hello', '/')
+    config.add_route('cwmp', '/')
     config.scan()
     app = config.make_wsgi_app()
     server = make_server('0.0.0.0', 8080, app)
