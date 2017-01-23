0e3d1e487dee38bca804e94ebde73c6de0fc3f82
Dario Protulipac <dprotulipac@win.vipnet.hr>
2017-01-23 10:32:25 +0100

    pylint happy

--- src/devreq.py
+++ src/devreq.py
@@ -1,10 +1,23 @@
 # -*- coding: utf-8 -*-
+"""
+Test function for sending connectio request
+"""
 import requests
 from device import cpe
 
 
-acsurl = 'http://localhost:10301'
-req = requests.Session().post(acsurl, cpe.tostring(), headers={'Content-Type': 'text/xml; charset=ISO-8859-1', 'SOAPAction': ''})
-for key in req.headers:
-    print(key + ': ' + req.headers[key])
-print(req.text)
+ACSURL = 'http://localhost:10301'
+
+def send_request():
+    """
+    Send connection request to ACS server
+    """
+    req = requests.Session().post(ACSURL, cpe.tostring(),
+                                  headers={'Content-Type': 'text/xml; charset=ISO-8859-1',
+                                           'SOAPAction': ''})
+    for key in req.headers:
+        print(key + ': ' + req.headers[key])
+    print(req.text)
+
+if __name__ == '__main__':
+    send_request()
--- src/tstflask.py
+++ src/tstflask.py
@@ -1,19 +1,25 @@
 # -*- coding: utf-8 -*-
 
+"""
+Test flask application
+"""
+
 from flask import Flask
 from flask import request, Response, session
 from acs import response as acs_response
 from soap.parse import Device
 from device import cpe
-from lxml import etree
 
-app = Flask(__name__)
-app.config["SECRET_KEY"] = cpe.sessionID(32)
-app.secret_key = cpe.sessionID(32)
+_app = Flask(__name__)
+_app.config["SECRET_KEY"] = cpe.sessionID(32)
+_app.secret_key = cpe.sessionID(32)
 
 
-@app.route('/', methods=['GET', 'POST'])
+@_app.route('/', methods=['GET', 'POST'])
 def cwmp():
+    """ Return response to Device
+    and set session cookie
+    """
     dev = Device(request.data)
     rsp = acs_response.tostring(ID=dev.cwmpID)
     session['sessionID'] = dev.cwmpID
@@ -21,4 +27,4 @@ def cwmp():
 
 
 if __name__ == '__main__':
-      app.run(port=10301)
+    _app.run(port=10301)
